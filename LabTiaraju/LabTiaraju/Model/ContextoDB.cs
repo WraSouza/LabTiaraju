af212193f1d07828f679150d9c747c0cc9cc4194
Wladimir Ribeiro de Souza <wladimir.souza@laboratorio.br>
2021-05-26 15:54:13 -0300

    Adicionar arquivos de projeto.

---
+++ LabTiaraju/LabTiaraju/Model/ContextoDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace LabTiaraju.Model
+{
+    public class ContextoDB
+    {
+
+        public static bool ConexaoBanco()
+        {
+            bool resultadoConexao = false;
+            var stringConexao = "Server=localhost;Database=labtiaraju;Uid=root;Pwd=root";
+            //var stringConexao = "Server=localhost;Database=labtiaraju;Uid=root;Pwd=root;SslMode=Preferred";
+            MySqlConnection con = new MySqlConnection(stringConexao);
+            //string connection = "Server=localhost;Database=labtiaraju;Uid=root;Pwd= root";
+
+
+            try
+            {
+                con.Open();
+                resultadoConexao = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro = " + e.Message.ToString());
+            }
+            
+            return resultadoConexao;
+
+        }
+    }
+}
---
+++ LabTiaraju/LabTiaraju/Views/LoginView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LabTiaraju.Model;
+
+namespace LabTiaraju.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+
+            ConectaBanco();
+        }
+
+        private void ConectaBanco()
+        {
+            bool verificaConexao = ContextoDB.ConexaoBanco();
+            if (verificaConexao)
+            {
+                Application.Current.MainPage.DisplayAlert("Sucesso", "Conexão Feita", "OK");
+            }
+        }
+    }
+}
